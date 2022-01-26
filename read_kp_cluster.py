de2f25b63a63a70b6fcbf9f6b40bf9d7b9602ba7
saurabhjawa <jai_dev@macs-Mac-mini.local>
2022-01-26 20:38:52 +0530

    read_kp_cluster updated

--- read_kp_cluster.py
+++ read_kp_cluster.py
@@ -2,7 +2,7 @@
 from kafka import KafkaProducer
 import time
 
-fb=open('/Users/jai_dev/PycharmProjects/retail_poc/data/test_data/part-00000')
+fb=open('/user/saurabh/part-00000')
 
 producer = KafkaProducer(bootstrap_servers=['cdp01.itversity.com:2181,cdp02.itversity.com:2181,cdp03.itversity.com:2181'],
                          value_serializer=lambda x:bytes(x,'utf-8'))
