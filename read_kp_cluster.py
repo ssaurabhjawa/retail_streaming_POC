fd9a8cbbd20227511421edd5d442f814eb9ab200
saurabhjawa <jai_dev@macs-Mac-mini.local>
2022-01-27 05:08:39 +0530

    /home_/kafka

--- read_kp_cluster.py
+++ read_kp_cluster.py
@@ -5,6 +5,7 @@ import time
 fb=open('/home/saurabh/part-00000')
 
 producer = KafkaProducer(bootstrap_servers=['cdp01.itversity.com:2181,cdp02.itversity.com:2181,cdp03.itversity.com:2181/kafka'],
+                         api_version=(20, 2, 1),
                          value_serializer=lambda x:bytes(x,'utf-8'))
 producer.flush()
 
