a91f1ced905831e56817c350ed0042307cda824d
saurabhjawa <jai_dev@macs-Mac-mini.local>
2022-01-28 18:49:55 +0530

    python3 app.py

--- runme.sh
+++ runme.sh
@@ -5,8 +5,5 @@ export SRC_DIR=/home/${USER}/retail_json_data/
 export TGT_FOLDER=''
 export TGT_FILE_FORMAT=''
 kafka-topics --create --partitions 1--replication-factor 1--topic quickstart-events --bootstrap-server cdp01.itversity.com:2181,cdp02.itversity.com:2181,cdp03.itversity.com:2181/kafka
-spark-submit \
---master yarn \
---conf spark.ui.port=0 \
-app.py
+python3 app.py
 
