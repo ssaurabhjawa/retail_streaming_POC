0634cd751d41ffc0b660db033072c089ed9e2b37
saurabhjawa <jai_dev@macs-Mac-mini.local>
2022-01-28 20:46:58 +0530

    1

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def main():
     tgt_dir=os.environ.get('TGT_DIR')
     tgt_file_format=os.environ.get('TGT_FILE_FORMAT')
     read_send_message(src_dir)
-    spark_consumer(env, 'retail_streaming_consumer')
     kafka_consumer(env, 'retail_k_consumer')
+    spark_consumer(env, 'retail_streaming_consumer')
 
 if __name__ == '__main__':
     main()
