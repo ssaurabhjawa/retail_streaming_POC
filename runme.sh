a9fea03f025a99b6b9a4ee63c02ae11bddc7e5d6
saurabhjawa <jai_dev@macs-Mac-mini.local>
2022-01-28 18:09:12 +0530

    07_final

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
 export HADOOP_CONF_DIR="/etc/hadoop/conf"
 export PYSPARK_PYTHON=python3
 export ENVIRON=PROD
-export SRC_DIR=/user/${user}/retail_json_data/
+export SRC_DIR=/user/${USER}/retail_json_data/
 export TGT_FOLDER=''
 export TGT_FILE_FORMAT=''
 kafka-topics --create --partitions 1--replication-factor 1--topic quickstart-events --bootstrap-server cdp01.itversity.com:2181,cdp02.itversity.com:2181,cdp03.itversity.com:2181/kafka
