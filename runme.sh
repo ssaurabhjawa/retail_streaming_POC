79da72a30de34328343b060b66ded52e4ab9b4a5
saurabhjawa <jai_dev@macs-Mac-mini.local>
2022-02-01 17:07:04 +0530

    runme.sh

--- runme.sh
+++ runme.sh
@@ -2,9 +2,7 @@ export HADOOP_CONF_DIR="/etc/hadoop/conf"
 export PYSPARK_PYTHON=python3
 export ENVIRON=PROD
 export SRC_DIR=/home/${USER}/retail_json_data/
-spark-submit \
---master yarn \
---properties-file /home/{USER}/retail_streaming_POC/configs/streaming_retail_data.conf \
-app.py
+
+spark-submit --master yarn --properties-file /home/{USER}/retail_streaming_POC/configs/streaming_retail_data.conf app.py
 
 
