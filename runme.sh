0ac93eec6f475df9f74fab43b19444dbb9acfb00
saurabhjawa <jai_dev@macs-Mac-mini.local>
2022-02-01 17:02:54 +0530

    runme.sh

--- runme.sh
+++ runme.sh
@@ -4,7 +4,7 @@ export ENVIRON=PROD
 export SRC_DIR=/home/${USER}/retail_json_data/
 spark-submit \
 --master yarn \
---properties-file /home/{USER}/retail_streaming_data/configs/streaming_retail_data.conf \
+--properties-file /home/{USER}/retail_streaming_POC/configs/streaming_retail_data.conf \
 app.py
 
 
