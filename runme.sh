0b9250ee6055dc6735d479c38b881e4a66310d0e
saurabhjawa <jai_dev@macs-Mac-mini.local>
2022-02-01 18:46:22 +0530

    all files

--- runme.sh
+++ runme.sh
@@ -3,6 +3,4 @@ export PYSPARK_PYTHON=python3
 export ENVIRON=PROD
 export SRC_DIR=/home/${USER}/retail_json_data/
 
-spark-submit --master yarn --properties-file /home/{USER}/retail_streaming_POC/configs/streaming_retail_data.conf app.py
-
-
+spark-submit --master yarn --properties-file /home/{USER}/retail_streaming_POC/configs/streaming_retail_data.conf git app.py
